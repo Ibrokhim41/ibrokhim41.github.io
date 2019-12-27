9d5a546a4c95e033f94a547a1acefeeb7b89ec6c
Ibrokhim41 <39386544+Ibrokhim41@users.noreply.github.com>
2019-12-27 11:01:16 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@ $(document).ready(function() {
  // executes when HTML-Document is loaded and DOM is ready
 console.log("document is ready");
   
-  
+  var width = $("body").innerWidth();
+
   $('#navbars').on('click', function () {
   $('.header_top_nav').addClass('open')
   $('.header_top_nav').removeClass('close_nav')
@@ -11,6 +12,41 @@ console.log("document is ready");
   $('.header_top_nav').addClass('close_nav')
   $('.header_top_nav').removeClass('open')
   });
+
+  if (width < 992) {
+  $('#dropdown1').on('click', function() {
+    // $('#dropdown_content1').toggleClass('mobshow');
+    $('#dropdown_content1').slideToggle();
+  });
+  $('#dropdown2').on('click', function() {
+    // $('#dropdown_content1').toggleClass('mobshow');
+    $('#dropdown_content2').slideToggle();
+  });
+  $('#dropdown3').on('click', function() {
+    // $('#dropdown_content1').toggleClass('mobshow');
+    $('#dropdown_content3').slideToggle();
+  });
+  $('#dropdown4').on('click', function() {
+    // $('#dropdown_content1').toggleClass('mobshow');
+    $('#dropdown_content4').slideToggle();
+  });
+  $('#dropdown5').on('click', function() {
+    // $('#dropdown_content1').toggleClass('mobshow');
+    $('#dropdown_content5').slideToggle();
+  });
+  $('#dropdown6').on('click', function() {
+    // $('#dropdown_content1').toggleClass('mobshow');
+    $('#dropdown_content6').slideToggle();
+  });
+  $('#dropdown7').on('click', function() {
+    // $('#dropdown_content1').toggleClass('mobshow');
+    $('#dropdown_content7').slideToggle();
+  });
+  $('#dropdown8').on('click', function() {
+    // $('#dropdown_content1').toggleClass('mobshow');
+    $('#dropdown_content8').toggleClass('mobd');
+  });
+}
   
 // document ready  
 });
